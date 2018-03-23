1c118160fb66b8ce6c4d7d367c3d38549a139f31
Lina-Maria <Lina-Maria@Lina.hfu-open.fu.hs-furtwangen.de>
2018-03-23 13:20:11 +0100

    aufgabe_0

---
+++ aufgabe_0/aufgabe_0.ts
@@ -0,0 +1,8 @@
+function Textfeld() {
+    var name= prompt ("Hallo, wie heißt du denn?", "");
+    if (name != null) {
+        document.getElementById("Hallo").innerHTML= "Hey" +" "+ name + "! Viel Spaß hier";
+                        }
+                        }
+
+//Funktioniert nur mit einer richtigen Verlinkung auf JS
